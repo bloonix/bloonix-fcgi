46e63782aac06906845ae60432696e371a3c403b
Jonny Schulz <support@bloonix.de>
2015-02-28 19:16:40 +0100

    Splitted accecpt() into multiple methods.

--- lib/Bloonix/FCGI.pm
+++ lib/Bloonix/FCGI.pm
@@ -17,7 +17,7 @@ use constant PARENT_PID => $$;
 __PACKAGE__->mk_accessors(qw/socket request done children ipc log json/);
 __PACKAGE__->mk_counters(qw/ttlreq ttlbyt/);
 
-our $VERSION = "0.4";
+our $VERSION = "0.5";
 
 sub new {
     my $class = shift;
@@ -64,6 +64,7 @@ sub init {
     $self->{kill_procs} = 0;
     $self->{next_kill} = time;
     $self->{json} = JSON->new->utf8;
+    $SIG{__DIE__} = sub { $self->log->dump(fatal => @_) };
 }
 
 sub daemonize {
@@ -324,14 +325,46 @@ sub statm {
 
 sub accept {
     my $self = shift;
-    my $server_status = $self->{server_status};
 
-    # Reset memory statistics
-    $self->{statm} = undef;
+    # Bloonix::Heaven->accept stops if undef is returned and the outer
+    # loop is used to prevent the child to die on SIGUSR1 and SIGUSR2.
+    while ($self->done == 0) {
+        $self->pre_accept;
+        $self->ipc->set($$, status => "W", time => time);
+
+        while ($self->done == 0 && $self->request->Accept() >= 0) {
+            $self->ipc->set($$, status => "R", time => time);
+            my $req = Bloonix::FCGI::Request->new();
+
+            $self->ipc->set($$,
+                status  => "P",
+                ttlreq  => $self->ttlreq(1),
+                client  => $req->remote_addr,
+                request => join(" ", $req->request_method, $req->request_uri),
+            );
+
+            if ($self->is_server_status($req)) {
+                $self->ipc->set($$, status => "W", time => time);
+                next;
+            }
+
+            return $req;
+        }
+    }
 
     if ($self->done) {
         exit 0;
     }
+
+    $self->ipc->set($$, status => "N", time => time);
+    return undef;
+}
+
+sub pre_accept {
+    my $self = shift;
+
+    # Reset memory statistics
+    $self->{statm} = undef;
 
     if ($self->{max_requests} && $self->ttlreq > $self->{max_requests}) {
         $self->log->warning("$$ reached max_requests of $self->{max_requests} - good bye");
@@ -347,49 +380,125 @@ sub accept {
         );
         exit 0;
     }
+}
 
-    $self->ipc->set($$, status => "W", time => time);
+sub is_server_status {
+    my ($self, $req) = @_;
+    my $server_status = $self->{server_status};
 
-    # Bloonix::Heaven->accept stops if undef is returned and the outer
-    # loop is used to prevent the child to die on SIGUSR1 and SIGUSR2.
-    while ($self->done == 0) {
-        while ($self->done == 0 && $self->request->Accept() >= 0) {
-            $self->ipc->set($$, status => "R", time => time);
-            my $req = Bloonix::FCGI::Request->new();
+    if ($server_status->{enabled} eq "yes" && $req->path_info eq $server_status->{location}) {
+        my $authkey = $req->param("authkey") // "";
+        my $addr = $req->remote_addr || "n/a";
+        my $allow_from = $server_status->{allow_from};
 
-            $self->ipc->set($$,
-                status  => "P",
-                ttlreq  => $self->ttlreq(1),
-                client  => $req->remote_addr,
-                request => join(" ", $req->request_method, $req->request_uri),
-            );
+        if ($allow_from->{all} || $allow_from->{$addr} || ($server_status->{authkey} && $server_status->{authkey} eq $authkey)) {
+            $self->log->info("server status request from $addr - access allowed");
 
-            if ($server_status->{enabled} eq "yes" && $req->path_info eq $server_status->{location}) {
-                my $pretty = $req->param("pretty") // "false";
-                my $json = $pretty eq "true" ? $self->json->pretty : $self->json;
-                my $addr = $req->remote_addr || "n/a";
-                my $allow_from = $server_status->{allow_from};
-
-                if ($allow_from->{all} || $allow_from->{$addr}) {
-                    print "Content-Type: application/json\n\n";
-                    print $json->encode({ status => "ok", data => $self->ipc->proc_status });
-                    $self->log->info("server status request from $addr - access allowed");
-                    next;
-                }
-
-                $self->log->warning("server status request from $addr - access denied");
+            if (defined $req->param("plain")) {
+                $self->print_plain_server_status;
+            } else {
+                $self->print_json_server_status($req);
             }
+        } else {
+            $self->log->warning("server status request from $addr - access denied");
+        }
 
-            return $req;
+        return 1;
+    }
+
+    return undef;
+}
+
+sub generate_server_statistics {
+    my $self = shift;
+    my $procs = $self->ipc->proc_status;
+
+    my $stats = {
+        procs => [], ttlreq => 0,
+        S => 0, W => 0, R => 0,
+        P => 0, N => 0
+    };
+
+    foreach my $proc (@$procs) {
+        my $status = $proc->{status} || "F";
+        $stats->{ttlreq} += $proc->{ttlreq};
+        $stats->{$status}++;
+
+        if ($proc->{pid}) {
+            push @{$stats->{procs}}, $proc;
         }
     }
 
-    if ($self->done) {
-        exit 0;
-    }
+    return $stats;
+}
 
-    $self->ipc->set($$, status => "N", time => time);
-    return undef;
+sub print_plain_server_status {
+    my $self = shift;
+    my $stats = $self->generate_server_statistics;
+    my $format = "%6s  %6s  %15s  %19s  %39s  %s\n";
+    my @content;
+
+    print "Content-Type: text/plain\n\n";
+
+    print "* Column description\n\n";
+    print "    PID     - The process id.\n";
+    print "    STATUS  - The current status of the process.\n";
+    print "    TTLREQ  - The total number of processed requests.\n";
+    print "    TIME    - The time when the last request was processed.\n";
+    print "    CLIENT  - The IP address of the client that is/were processed.\n";
+    print "    REQUEST - The request of the client that is/were processed.\n\n";
+
+    print "* Status description\n\n";
+    print "    S - Starting up\n";
+    print "    W - Waiting for connection\n";
+    print "    R - Reading request\n";
+    print "    P - Processing request\n";
+    print "    N - No request received\n\n";
+    print "    If the status is in RWN then the columns TIME, CLIENT and REQUEST\n";
+    print "    shows information about the last request the process processed.\n\n";
+
+    print "* Statistics\n\n";
+    print "    Server time: ", $self->timestamp(time), "\n\n";
+    printf "    Total requests procesesed: $stats->{ttlreq}\n\n";
+    printf "%8s worker starting up\n", $stats->{S};
+    printf "%8s worker waiting for incoming request\n", $stats->{W};
+    printf "%8s worker reading request\n", $stats->{R};
+    printf "%8s worker procesing request\n", $stats->{P};
+    printf "%8s worker in status n/a\n", $stats->{N};
+    printf "%8s free slots available\n\n", $stats->{F};
+
+    print "* Process list\n\n";
+    printf $format, qw(PID STATUS TTLREQ TIME CLIENT REQUEST);
+
+    foreach my $proc (@{$stats->{procs}}) {
+        printf $format,
+            $proc->{pid},
+            $proc->{status},
+            $proc->{ttlreq},
+            $self->timestamp($proc->{time}),
+            $proc->{client},
+            $proc->{request};
+    }
+}
+
+sub print_json_server_status {
+    my ($self, $req) = @_;
+
+    my $json = defined $req->param("pretty")
+        ? JSON->new->pretty(1)
+        : JSON->new->pretty(0);
+
+    print "Content-Type: application/json\n\n";
+    print $json->encode({ status => "ok", data => $self->ipc->proc_status });
+}
+
+sub timestamp {
+    my $self = shift;
+    my $time = shift || time;
+    my @time = (localtime($time))[reverse 0..5];
+    $time[0] += 1900;
+    $time[1] += 1;
+    return sprintf "%04d-%02d-%02d %02d:%02d:%02d", @time[0..5];
 }
 
 sub attach {
@@ -494,10 +603,10 @@ sub validate_server_status {
             type => Params::Validate::SCALAR,
             default => "127.0.0.1"
         },
-        #auth_string => {
-        #    type => Params::Validate::SCALAR,
-        #    optional => 1,
-        #}
+        authkey => {
+            type => Params::Validate::SCALAR,
+            optional => 1,
+        }
     });
 
     $options{allow_from} =~ s/\s//g;
